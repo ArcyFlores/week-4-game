932aad2db74a278e6672efafbab55c054b8b83e3
ArcyFlor <arcylizet@ucla.edu>
2016-07-27 10:08:27 -0700

    changes

--- javascript/game.js
+++ javascript/game.js
@@ -1,4 +1,5 @@
  $(document).ready(function() {
+ 
  	//The player will be shown a random number at the start of the game.
  	var numToGuess = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
  	$("#matchNumber").html(numToGuess);
@@ -8,8 +9,8 @@
  	var losses = 0;
  	var counter = 0;
  	$("#userScore").html(counter);
- 	
- 	// Each crystal should have a random hidden value between 1 - 12
+
+  // Each crystal should have a random hidden value between 1 - 12
  	var crystalValue1=(Math.floor(Math.random() * (12 - 1 + 1)) + 1);
  	var crystalValue2=(Math.floor(Math.random() * (12 - 1 + 1)) + 1);
  	var crystalValue3=(Math.floor(Math.random() * (12 - 1 + 1)) + 1);
@@ -18,11 +19,10 @@
  	console.log(crystalValue2);
  	console.log(crystalValue3);
  	console.log(crystalValue4);
- 	
- 	
+
  	
  	// When the player clicks on a crystal it will add a specific amount of points to the player's total score.
-	
+
     $('.crystalImage1').on('click', function(){
    		counter = counter + crystalValue1;
    		console.log(counter);
@@ -31,13 +31,11 @@
     	alert('You WON!');
     	wins++;
     }
+
     else if (counter > numToGuess){
     	alert("You LOST");
     	losses++;
     }
-
-	$("#win").html(wins);
- 	$("#lose").html(losses);
 
     });
 
@@ -53,6 +51,7 @@
     	alert("You LOST");
     	losses++;
     }
+
     });
 
     $('.crystalImage3').on('click', function(){
@@ -83,14 +82,9 @@
     }
     });
 
+  $("#win").html(wins);
+  $("#lose").html(losses);
+
+
  });
 
-// * The player wins the game if their total score matches the random number and the player loses the game if their total score goes above the random number.
-// * The amount of points each crystal adds is not shown to the player, but their total score is displayed and updated after each crystal is clicked.
-// * The number of games won and lost will be displayed.
-// * If the player successfully matches their total score to the random number they get 1 win, if the player's total score is above the random number the player get 1 loss, either way the game restarts.
-// * When the game is restarted, the player will be shown a new random number, all the crystals will now have 4 different hidden values, and the player's total score is reset to 0.
-
-// ##### Game design note:
-// ###### The random number shown at the start of the game should be between 19 - 120
-// ###### Each crystal should have a random hidden value between 1 - 12
